2fd7fbff18b91cdd444d159f88f36c70b6d0628e
Marcos Paulo <marcos_pa32@hotmail.com>
2021-07-21 12:41:55 -0300

    Sevices e Promises

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertaService } from '../ofertas.services';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertaService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public ofertas!: Oferta []
+
+  constructor(private ofertaService: OfertaService) { }
 
   ngOnInit(): void {
+    //this.ofertas = this.ofertaService.getOfertas()
+    //console.log(this.ofertas)
+    this.ofertaService.getOfertas2().then(
+      (ofertas : Oferta[])=>{console.log('Funfou depois de 3sec'),this.ofertas = ofertas},
+      (param: any)=>{console.log(param)}
+      )
   }
 
 }
